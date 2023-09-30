a684e15736a9d69622a6eb28e5982ac83bca6071
Alex <juzzle@mail.ru>
2023-09-30 12:59:08 +0300

    GOVNO EBANOE

--- Home1/src/main/java/org/example/Main.java
+++ Home1/src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
-
+import static org.assertj.core.api.Assertions.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calc c = new Calc();
+//        testMaxValue();
+//        testDiscountValue();
+          testDiscountValueLess0();
     }
+
+    /***
+     * Метод проверки на не допустимую границу Double
+     */
+    public static void testMaxValue(){
+        Calc c = new Calc();
+        assertThat(c.calculateDiscount(Double.MAX_VALUE,0)).isEqualTo(Double.MAX_VALUE);
+    }
+    public static void  testDiscountValue(){
+        Calc c = new Calc();
+        assertThat(c.calculateDiscount(100,101)).isEqualTo(0);
+    }
+    public static void testDiscountValueLess0(){
+        Calc c = new Calc();
+        assertThat(c.calculateDiscount(100,-1)).isEqualTo(-100);
+    }
+
 }
