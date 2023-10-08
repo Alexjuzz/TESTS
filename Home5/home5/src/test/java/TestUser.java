60a1788bd95551450909f621fe22f0e4ca15cdfc
Alex <juzzle@mail.ru>
2023-10-08 23:55:30 +0300

    test5

--- Home5/home5/src/test/java/TestUser.java
+++ Home5/home5/src/test/java/TestUser.java
@@ -16,34 +16,43 @@ public class TestUser {
         user = new User("Vova", "Ivanov", comment, 912949);
 
     }
-
     @Test
     void testGetNameUser() {
+        // Проверяет, что имя пользователя не пустое.
         assertFalse(user.getFirstName().isEmpty());
     }
 
     @Test
     void testGetSecondNameUser() {
+        // Проверяет, что фамилия пользователя не пустая.
         assertFalse(user.getSecondName().isEmpty());
     }
 
     @Test
     void testGetNumberUser() {
-        assertFalse(user.getNumber() >=1  && user.getNumber() < 999999999);
+        // Проверяет, что номер пользователя находится в допустимом диапазоне.
+        assertFalse(user.getNumber() >= 1 && user.getNumber() < 999999999);
     }
+
     @Test
-    void testSetUserName(){
+    void testSetUserName() {
+        // Проверяет, что метод setFirstName корректно устанавливает имя пользователя.
         user.setFirstName("KOLA");
-        assertEquals("KOLA",user.getFirstName());
+        assertEquals("KOLA", user.getFirstName());
     }
+
     @Test
-    void testSetSecondNameUser(){
+    void testSetSecondNameUser() {
+        // Проверяет, что метод setSecondName корректно устанавливает фамилию пользователя.
         user.setSecondName("PETROV");
-        assertEquals("PETROV",user.getSecondName());
+        assertEquals("PETROV", user.getSecondName());
     }
+
     @Test
-    void testChangeNumber(){
+    void testChangeNumber() {
+        // Проверяет, что метод setNumber корректно изменяет номер пользователя.
         user.setNumber(49129392);
-        assertEquals(49129392,user.getNumber());
+        assertEquals(49129392, user.getNumber());
     }
+
 }
